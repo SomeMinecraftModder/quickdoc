3dfb8b439e306b231cd2c0aeeb37d8022a34ec0a
SomeMinecraftModder <staff@webgarbage.fr>
2022-01-11 18:45:48 +0100

    yes

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import sys
 
+
+def parse_document(file):
+    f = open(file).read()
+    parsed_document = f.split("\n")
+    for line in parsed_document:
+        if not line.startswith("[") and line.endswith("]"):
+            print("Error at line:%s")
+            print("Line not starting and ending with [ or ]")
+            exit(-1)
+    return parsed_document
+
+
 VERSION = "0.0.1"
 
-print(sys.argv)
 if not len(sys.argv) == 2:
     print("Have you tried putting a/only one file as a argument?")
+
+print("QuickDoc V." + str(VERSION))
+print("Starting parsing")
+document = parse_document(sys.argv[1])
